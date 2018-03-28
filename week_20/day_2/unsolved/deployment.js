79ac76a46e4976e4aa8fe1f8328f85059d9a8ffe
Gerardo Fernandez <gerardojose13@gmail.com>
2018-03-27 23:25:46 -0700

    pushes python files

---
+++ week_20/day_2/unsolved/deployment.js
@@ -0,0 +1,14 @@
+// THESE ARE THE INSTRUCTIONS TO DEPLOY THE APP TO HEROKU
+//ALSO USE create-express-react to get both package.JSON on the client and the server side.
+
+//Create the repo
+//FIRST RUN YARN BUILD - this will create the files and build folder that will eventually get uploaded to the cloent side.
+
+//THEN, CREATE THE mlab DATABASE - this will allow you to have a database for the app
+
+//-----------  on the terminal  -------------
+// $ heroku create
+// $ git push heroku master
+
+//if I want to updload the seed that was made for the app, I can run this command on the terminal
+// $ heroku run node (and the path to run that's present in the package Jason script). 
---
+++ week_21/python-unsolved/01-Console-IO/unsolved.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+###############################################################################
+#
+# BRIEF //
+#   Your firm is implementing its student management prototype in Python.
+#   It's a command-line program, which the registrar's office will use to
+#   add students to the database upon enrollment.
+#
+#   The program should prompt the user for a student's first name; last name;
+#   middle initial; physical address; email address; and phone number.
+#
+#   After each prompt, the program should wait for the user's input. 
+#
+#   Once the user has entered every piece of information, the program should
+#   print it all back to the console, and prompt the user to enter Y if the
+#   information is correct, or any other key otherwise.
+#
+#   For now, you should collect the user's response--i.e., y or otherwise--but
+#   don't worry about handling it. We'll get to that shortly.
+#
+###############################################################################
+
+firstName = input("Please tell me your first name: ")
+lastName = input("Please tell me your Last Name: ")
+midlInit = input("What is your middle initial?: ")
+address = input("and your address?: ")
+email = input("and your email?: ")
+phone = input("Lastly, what is your phone number?: ")
+
+print("=" * 20)
+print("Hello {0} {1}. {2}".format(firstName, midlInit, lastName))
+print("Your address is: {0}".format(address))
+print("And your email and phone number are: {0}, {1}".format(email, phone))
+print("=" * 20)
+
+correct = input("Is this information correct? y/n - ")
+
+
+# What function prints a message to the screen and waits for user input?
+# Use it here to collect a student's information--first name, last name, etc.
+###############################################################################
+#...Your Code Here...
+
+# Once you've gotten all of that, print it all back to the screen. 
+###############################################################################
+#...Your Code Here...
+
+# Then, use the same function you used to prompt users for information to ask 
+# them to confirm whether or not the information is correct. Save their 
+# response, but don't worry about doing anything with it yet!
+###############################################################################
+# ...Your Code Here...
